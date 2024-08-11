4cc589b645190d21ab4935e9322758a59f2e42ff
Dhruv <85575534+Ambi21@users.noreply.github.com>
2024-08-12 01:47:55 +0530

    Add files via upload

---
+++ ReverseString.py
@@ -0,0 +1,14 @@
+s = "a good   example"
+arr = s.split(" ")
+str1 = ''
+print(arr)
+# arr[:] = arr[::-1]
+
+# while (str1 in arr):
+#     arr.remove(str1)
+# print(arr)
+# for i in arr:
+#     str1 += i + " "
+# print(str1)
+# str1 = "abcdefg"
+# print(str1[:-1])
